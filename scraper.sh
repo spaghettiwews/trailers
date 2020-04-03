2e14bd94ecde2882f402675348efec29dbff197d
spaghettiwews <spaghettiwews@outlook.com>
2020-04-03 22:24:02 +0200

    update scraper.sh and workflow

--- scraper.sh
+++ scraper.sh
@@ -10,7 +10,7 @@ PAGE_URL_REGEX="https:\/\/[A-Za-z0-9\-\.\/\-]*"
 TRAILER_REGEX="<iframe src=\"https:\/\/www\.youtube\.com.+rel=0.+<\/iframe>"
 LAST_UPDATED=$(date +"%A,%e %B %Y @ %T")
 
-echo "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><script src='https://cdn.jsdelivr.net/npm/vanilla-lazyload@15.1.1/dist/lazyload.min.js'></script><title>Trailers | ${LAST_UPDATED}</title></head><body><style>iframe{position:relative!important;width:600px!important;height:320px!important;display:block;margin:1rem auto;}h2{max-width: 600px;margin: 2rem auto;font-family: monospace;}</style><h2>Last checked: ${LAST_UPDATED}</h2>" > index.html
+echo "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><script src='https://cdn.jsdelivr.net/npm/vanilla-lazyload@15.1.1/dist/lazyload.min.js'></script><title>Trailers | ${LAST_UPDATED}</title></head><body><style>iframe{position:relative!important;width:600px!important;height:320px!important;display:block;margin:1rem auto;}h2{max-width: 600px;margin: 2rem auto;font-family: monospace;}</style><h2>Last checked: ${LAST_UPDATED}</h2>" > public/index.html
 
 while [ $YEAR -ge $END_YEAR ]
 do
@@ -31,11 +31,11 @@ do
             xargs curl -s --compressed |
             grep -o -E "${TRAILER_REGEX}" |
             sed s/"<iframe src"/"<iframe data-src"/g |
-            cat >> index.html
+            cat >> public/index.html
         }
         ((MONTH--))
     done
     ((YEAR--))
 done
 
-echo '<script>document.addEventListener("DOMContentLoaded",function(){var lazyLoadInstance=new LazyLoad({elements_selector:"iframe"});});</script></body></html>' >> index.html
+echo '<script>document.addEventListener("DOMContentLoaded",function(){var lazyLoadInstance=new LazyLoad({elements_selector:"iframe"});});</script></body></html>' >> public/index.html
